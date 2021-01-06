2ecaa18e594ca40148097219addc3e6c2187d810
Adebambo Adenuga <adenugaadebambo41@gmail.com>
2021-01-07 00:39:50 +0100

    communioncc app

--- lib/screens/discover_screen.dart
+++ lib/screens/discover_screen.dart
@@ -9,34 +9,9 @@ class DiscoverScreen extends StatefulWidget {
 class _DiscoverScreenState extends State<DiscoverScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // Setting up background color
-      backgroundColor: mBackgroundColour,
-      body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.only(top: 20.0, left: 15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Discover",
-                style: TextStyle(
-                  fontSize: 35.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: "Montserrat",
-                ),
-              ),
-              Text(
-                "Discover more from C3",
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontFamily: "Montserrat",
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ],
-          ),
-        ),
+    return Container(
+      child: Center(
+        child: Text("Discover"),
       ),
     );
   }
