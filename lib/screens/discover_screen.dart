3be9f4d4d6f67186e695297630e3792a1648c421
Adebambo Adenuga <adenugaadebambo41@gmail.com>
2021-01-16 00:35:01 +0100

    discover view

--- lib/screens/discover_screen.dart
+++ lib/screens/discover_screen.dart
@@ -101,7 +101,204 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
                 child: Wrap(
                   children: [
                     Image.network(
-                      'https://res.cloudinary.com/communioncc/image/upload/c_fit,h_1500,w_1500/v1/cc_files/media_clipart/5fe3e001176c4_1608769537.jpg',
+                      'https://www.gilgalproperties.com/wp-content/uploads/2018/11/black-businessman-happy-expression_1194-2542.jpg',
+                      fit: BoxFit.fill,
+                    ),
+                    ListTile(
+                      minVerticalPadding: 15.0,
+                      title: Text(
+                        "Communion Outreach",
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w900,
+                          color: mPinkColour,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                      subtitle: Text(
+                        "\n At CommunionCC, we help you discover and fulfil your God-given purpose or vision in life. We are called to fulfil our destinies and to help others fulfil their destinies in Christ",
+                        style: TextStyle(
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.w200,
+                          fontFamily: 'Montserrat',
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                  ],
+                ),
+                elevation: 5,
+                margin: EdgeInsets.all(10),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(15.0),
+              child: Text(
+                "FIND EVENTS",
+                style: TextStyle(
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.w900,
+                  fontFamily: 'Montserrat',
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 100.0,
+              child: Card(
+                semanticContainer: true,
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Wrap(
+                  children: [
+                    Image.network(
+                      'https://livingwellaware.com/wp-content/uploads/2020/03/Happy.jpg',
+                      fit: BoxFit.fill,
+                    ),
+                    ListTile(
+                      minVerticalPadding: 15.0,
+                      title: Text(
+                        "Core Interest Group",
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w600,
+                          color: mPinkColour,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                      subtitle: Text(
+                        "\n Learn more about our church and how you can get involved",
+                        style: TextStyle(
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.w200,
+                          fontFamily: 'Montserrat',
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                  ],
+                ),
+                elevation: 5,
+                margin: EdgeInsets.all(10),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(15.0),
+              child: Text(
+                "JOIN A TEAM",
+                style: TextStyle(
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.w900,
+                  fontFamily: 'Montserrat',
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 100.0,
+              child: Card(
+                semanticContainer: true,
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Wrap(
+                  children: [
+                    Image.network(
+                      'https://www.gilgalproperties.com/wp-content/uploads/2018/11/black-businessman-happy-expression_1194-2542.jpg',
+                      fit: BoxFit.fill,
+                    ),
+                    ListTile(
+                      minVerticalPadding: 15.0,
+                      title: Text(
+                        "eChurch",
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w900,
+                          color: mPinkColour,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                      subtitle: Text(
+                        "\n At CommunionCC, we help you discover and fulfil your God-given purpose or vision in life. We are called to fulfil our destinies and to help others fulfil their destinies in Christ",
+                        style: TextStyle(
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.w200,
+                          fontFamily: 'Montserrat',
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                  ],
+                ),
+                elevation: 5,
+                margin: EdgeInsets.all(10),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 100.0,
+              child: Card(
+                semanticContainer: true,
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Wrap(
+                  children: [
+                    Image.network(
+                      'https://livingwellaware.com/wp-content/uploads/2020/03/Happy.jpg',
+                      fit: BoxFit.fill,
+                    ),
+                    ListTile(
+                      minVerticalPadding: 15.0,
+                      title: Text(
+                        "New Here?",
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w600,
+                          color: mPinkColour,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                      subtitle: Text(
+                        "\n Learn more about our church and how you can get involved",
+                        style: TextStyle(
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.w200,
+                          fontFamily: 'Montserrat',
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                  ],
+                ),
+                elevation: 5,
+                margin: EdgeInsets.all(10),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(15.0),
+              child: Text(
+                "GET STARTED",
+                style: TextStyle(
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.w900,
+                  fontFamily: 'Montserrat',
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 100.0,
+              child: Card(
+                semanticContainer: true,
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Wrap(
+                  children: [
+                    Image.network(
+                      'https://www.gilgalproperties.com/wp-content/uploads/2018/11/black-businessman-happy-expression_1194-2542.jpg',
                       fit: BoxFit.fill,
                     ),
                     ListTile(
