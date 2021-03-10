2646a9a8db4efd1ae605dbc89dabc69cec34e91c
Adebambo Adenuga <adenugaadebambo41@gmail.com>
2021-03-10 19:14:14 +0100

    paystack page

--- lib/screens/paystack_screen.dart
+++ lib/screens/paystack_screen.dart
@@ -32,106 +32,108 @@ class _PaystackPageState extends State<PaystackPage> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 60.0),
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Image.asset(
-                    "assets/paystack_blue.png",
-                    width: MediaQuery.of(context).size.width - 50,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Form(
-                child: Column(
+          child: GestureDetector(
+            child: Column(
+              children: [
+                Row(
                   children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "First & Last Name",
-                      ),
+                    Image.asset(
+                      "assets/paystack_blue.png",
+                      width: MediaQuery.of(context).size.width - 50,
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                      child: TextFormField(
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
                         decoration: InputDecoration(
                           border: OutlineInputBorder(),
-                          labelText: "Email Address",
+                          labelText: "First & Last Name",
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: "Amount",
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Email Address",
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                      child: Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 4),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.grey, width: 1),
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                        child: DropdownButton(
-                          hint: Text("Purpose of Giving"),
-                          icon: Icon(Icons.arrow_drop_down),
-                          iconSize: 30,
-                          isExpanded: true,
-                          value: _purpose,
-                          onChanged: (value) {
-                            setState(() {
-                              _purpose = value;
-                            });
-                          },
-                          items: purposeItem.map((valueItem) {
-                            return DropdownMenuItem(
-                              value: valueItem,
-                              child: Text(valueItem),
-                            );
-                          }).toList(),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Amount",
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                      child: RaisedButton(
-                        onPressed: () {},
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0),
                         child: Container(
-                          width: MediaQuery.of(context).size.width,
-                          child: Center(
-                            child: Text(
-                              "Give",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: "Roboto",
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey, width: 1),
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                          child: DropdownButton(
+                            hint: Text("Purpose of Giving"),
+                            icon: Icon(Icons.arrow_drop_down),
+                            iconSize: 30,
+                            isExpanded: true,
+                            value: _purpose,
+                            onChanged: (value) {
+                              setState(() {
+                                _purpose = value;
+                              });
+                            },
+                            items: purposeItem.map((valueItem) {
+                              return DropdownMenuItem(
+                                value: valueItem,
+                                child: Text(valueItem),
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                        child: RaisedButton(
+                          onPressed: () {},
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            child: Center(
+                              child: Text(
+                                "Give",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: "Roboto",
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 50.0),
-                      child: Image.asset(
-                        "assets/paystack_card.png",
-                        fit: BoxFit.cover,
-                        width: double.infinity,
+                      Padding(
+                        padding: EdgeInsets.only(top: 50.0),
+                        child: Image.asset(
+                          "assets/paystack_card.png",
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
